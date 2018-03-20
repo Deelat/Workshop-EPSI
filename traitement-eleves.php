24ad1a76d55e202eab56d8eca6e93f1da786541a
POURCEL Antoine <antoine.pourcel@epsi.fr>
2018-03-20 16:48:31 +0100

    Mise àjour form inscription

--- traitement-eleves.php
+++ traitement-eleves.php
@@ -11,11 +11,8 @@ $email = $_POST['mail'];
 $password = md5($_POST['password']);
 
 $req = 'INSERT INTO eleves (nom, prenom, ddn, ecole, annee, mail, mdp) VALUES ("'.$firstname.'", "'.$lastname.'", "'.$dob.'", "'.$school.'", "'.$year.'", "'.$email.'", "'.$password.'")';
-var_dump($req);
 
 $res = mysqli_query($linkDB, $req);
-
-var_dump($linkDB);
 
 mysqli_errno($linkDB);
  ?>
