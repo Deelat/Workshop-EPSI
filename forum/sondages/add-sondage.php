38529802774fefc2a6fcf6bb17175d80f6be5b15
POURCEL Antoine <antoine.pourcel@epsi.fr>
2018-03-22 14:13:16 +0100

    Update sondage

--- forum/sondages/add-sondage.php
+++ forum/sondages/add-sondage.php
@@ -8,13 +8,12 @@ $titre = filter_var($_POST['titre'],FILTER_SANITIZE_STRING);
 $time = date("Y-m-d H:i:s");
 
 $req = "INSERT INTO sondage (nb_choix, titre, auteur, date_time) VALUES ('".$nb_choix."', '".$titre."', '".session_id()."', '".$time."')";
-var_dump($req);
 
 $res = mysqli_query($link, $req);
 $query = "SELECT id_sondage FROM sondage WHERE titre = '".$titre."' AND date_time = '".$time."';";
 $result = mysqli_query($link, $query);
 $id_sondage = mysqli_fetch_row($result);
-var_dump($id_sondage);
+
 
 for ($i=1; $i <= $_POST['nlignes']; $i++) {
 $titre = filter_var($_POST['option'.$i], FILTER_SANITIZE_STRING);
@@ -22,7 +21,7 @@ $req = "INSERT INTO optn_sondage (choix, id_sondage) VALUES ('".$titre."', '".$i
 
 $res = mysqli_query($link, $req);
 }
-var_dump($res);
+
 if (mysqli_errno($link)) {
   die(mysqli_error($link));
 }
